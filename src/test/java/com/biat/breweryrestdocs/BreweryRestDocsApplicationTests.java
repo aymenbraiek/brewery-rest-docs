c1f815ea369c25c9e576442c721dd7e18908b026
aymenbraiek <aymen.braiek@esprit.tn>
2020-04-09 19:53:01 +0100

    initial project

---
+++ src/test/java/com/biat/breweryrestdocs/BreweryRestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biat.breweryrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryRestDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
